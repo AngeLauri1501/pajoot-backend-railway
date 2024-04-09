d5bd25c5e5f3cb22957eab1f3a4589017191180e
AngeLauri1501 <126652766+AngeLauri1501@users.noreply.github.com>
2024-04-09 09:41:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -598,7 +598,7 @@ io.on('connection', (socket) => {
             })
             
             // Eliminar el juego de la lista de juegos
-            games.removeGame(gameWithDisconnectedHost.hostId);
+            games.removeGame(gameWithDisconnectedHost.pin);
             
             console.log(`Game with PIN ${gameWithDisconnectedHost.pin} cancelled due to host disconnection.`);
         }
